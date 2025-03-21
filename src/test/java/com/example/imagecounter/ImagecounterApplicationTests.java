1ef8f9528952afaf01f1b5331a0aa516e5c40d08
Emigarsan <emilio@franklin.uv.es>
2025-03-21 02:05:57 +0100

    Primera versión de ImageCounter

---
+++ src/test/java/com/example/imagecounter/ImagecounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imagecounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagecounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
